9197f199a89b27170548eb475cd50d135d17c2de
Cayden Jacobs <cayden.jacobs@yahoo.com>
2017-09-07 09:17:21 -0400

    Added first feature test

---
+++ spec/features/user_visits_homepage_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+
+RSpec.feature "User visits homepage" do
+  scenario "successfully and sees a logo" do
+    visit root_path
+    expect(page).to have_content "Girl Devs"
+  end
+end
