d889f363294459738fc7b26c82ff5722d1a256be
Cayden Jacobs <cayden.jacobs@yahoo.com>
2017-09-07 13:46:50 -0400

    Added macro for sign in feature; this happened with previous commit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -78,6 +78,9 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
+  # Config user module
+  config.include Features, type: :feature
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
